975848411899a24dc855c07864c767900f58e58d
Austin Maddison <Austin.madison10152@gmail.com>
2024-06-27 14:13:01 +0700

    Update UserDTO.java

--- src/main/java/io/muzoo/ssc/springwebapp/UserDTO.java
+++ src/main/java/io/muzoo/ssc/springwebapp/UserDTO.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Set;
+
 // POJO
 // Plain Old Java Object
 @Getter
@@ -13,8 +15,40 @@ import lombok.Setter;
 @NoArgsConstructor
 public class UserDTO {
 
-    private String username;
+    private String username, email, password, firstName, lastName, displayName, profilePicture, phoneNumber, biography;
+    private int age;
+    private double height;
+    private Set<String> preferences, dislikes;
 
-    private String name;
+    public UserDTO(String username, String email, String password, String firstName, String lastName, int age, double height, String displayName, String profilePicture, String phoneNumber, String biography, Set<String> preferences, Set<String> dislikes) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.height = height;
+        this.displayName = displayName;
+        this.profilePicture = profilePicture;
+        this.phoneNumber = phoneNumber;
+        this.biography = biography;
+        this.preferences = preferences;
+        this.dislikes = dislikes;
+    }
 
+    public UserDTO(User user) {
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.age = user.getAge();
+        this.height = user.getHeight();
+        this.displayName = user.getDisplayName();
+        this.profilePicture = user.getProfilePicture();
+        this.phoneNumber = user.getPhoneNumber();
+        this.biography = user.getBiography();
+        this.preferences = user.getPreferences();
+        this.dislikes = user.getDislikes();
+    }
 }
