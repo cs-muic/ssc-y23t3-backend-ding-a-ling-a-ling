be628b49bbaecba24d03006d1ebf002532b06ee6
Austin Maddison <Austin.madison10152@gmail.com>
2024-06-27 14:14:20 +0700

    Update UserService.java

--- src/main/java/io/muzoo/ssc/springwebapp/UserService.java
+++ src/main/java/io/muzoo/ssc/springwebapp/UserService.java
@@ -20,18 +20,59 @@ public class UserService {
     }
 
     public UserDTO getProfile(String username) {
-        return new UserDTO(username, username);
+        User user = userRepository.findByUsername(username);
+        if (user != null) {
+            return new UserDTO(user);
+        }
+        return null;
     }
 
-    public String createUser(String username, String name, String address, String group) {
+    public String createUser(UserDTO userDTO) {
         User user = new User();
-        user.setUsername(username);
-        user.setAddress(address);
-        // make this setting the set in this user by gathered the list of dislikes they've chosen or smth
-//        user.setDislikes();
+        user.setUsername(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setAge(userDTO.getAge());
+        user.setHeight(userDTO.getHeight());
+        user.setDisplayName(userDTO.getDisplayName());
+        user.setProfilePicture(userDTO.getProfilePicture());
+        user.setPhoneNumber(userDTO.getPhoneNumber());
+        user.setBiography(userDTO.getBiography());
+        user.setPreferences(userDTO.getPreferences());
+        user.setDislikes(userDTO.getDislikes());
 
         userRepository.save(user);
-        return "OK";
+        return String.format("Created user %s successfully", userDTO.getUsername());
+    }
+
+    public String updateUser(UserDTO userDTO) {
+        User user = userRepository.findByUsername(userDTO.getUsername());
+        if (user != null) {
+            user.setUsername(userDTO.getUsername());
+            user.setEmail(userDTO.getEmail());
+            user.setPassword(userDTO.getPassword());
+            user.setFirstName(userDTO.getFirstName());
+            user.setLastName(userDTO.getLastName());
+            user.setAge(userDTO.getAge());
+            user.setHeight(userDTO.getHeight());
+            user.setDisplayName(userDTO.getDisplayName());
+            user.setProfilePicture(userDTO.getProfilePicture());
+            user.setPhoneNumber(userDTO.getPhoneNumber());
+            user.setBiography(userDTO.getBiography());
+            user.setPreferences(userDTO.getPreferences());
+            user.setDislikes(userDTO.getDislikes());
+
+            userRepository.save(user);
+            return String.format("Updated user %s successfully", userDTO.getUsername());
+        }
+        return "User not found";
+    }
+
+    public Set<User> findUsersWithSimilarDislikes(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        return userRepository.findUsersWithCommonDislikes(user.getDislikes()); // Assuming this method is implemented in UserRepository
     }
 
     public Set<User> findUsersWithSimilarDislikes(Long userId) {
