b21d78376bcd71c7c2c0afade41c67ec0294315a
harrythe6arry <voravit.sri@student.mahidol.edu>
2024-07-02 21:44:15 +0700

    Uncomment methods that are not working

--- src/main/java/io/muzoo/ssc/springwebapp/controller/TestController.java
+++ src/main/java/io/muzoo/ssc/springwebapp/controller/TestController.java
@@ -38,101 +38,101 @@ import java.util.Set;
 //    }
 //}
 
-@RestController
-@RequestMapping("/api")
-public class TestController {
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private SimpleService simpleService;
-
-    @GetMapping("/user/{id}")
-    public User getName(@PathVariable Long id) {
-        return userService.getUser(id);
-    }
-
-    @PostMapping("/user/create")
-    @PreAuthorize("hasRole('USER')")
-    public String createUser(
-            @RequestParam String username,
-            @RequestParam String firstname,
-            @RequestParam String lastname,
-            @RequestParam String address,
-            @RequestParam String email,
-            @RequestParam String password,
-            @RequestParam String phone,
-            @RequestParam int age,
-            @RequestParam double height,
-            @RequestParam String displayName,
-            @RequestParam String profilePicture,
-            @RequestParam String contact,
-            @RequestParam String biography,
-            @RequestParam Set<String> preferences,
-            @RequestParam Set<String> dislikes
-    ) {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUsername(username);
-        userDTO.setEmail(email);
-        userDTO.setPassword(password);
-        userDTO.setFirstName(firstname);
-        userDTO.setLastName(lastname);
-        userDTO.setAddress(address);
-        userDTO.setAge(age);
-        userDTO.setHeight(height);
-        userDTO.setDisplayName(displayName);
-        userDTO.setProfilePicture(profilePicture); // should be the bucket url
-        userDTO.setContact(contact);
-        userDTO.setPhoneNumber(phone);
-        userDTO.setBiography(biography);
-        userDTO.setPreferences(preferences);
-        userDTO.setDislikes(dislikes);
-        return userService.createUser(userDTO);
-    }
-
-    @PostMapping("/api/user/update") //TODO: can change the para, to restrict user to change stuff
-    public String updateUser(
-            @RequestParam String username,
-            @RequestParam String firstname,
-            @RequestParam String lastname,
-            @RequestParam String address,
-            @RequestParam String email,
-            @RequestParam String password,
-            @RequestParam String phoneNumber,
-            @RequestParam double height,
-            @RequestParam String displayName,
-            @RequestParam String profilePicture,
-            @RequestParam String contact,
-            @RequestParam String biography,
-            @RequestParam Set<String> preferences,
-            @RequestParam Set<String> dislikes
-    ) {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUsername(username);
-        userDTO.setEmail(email);
-        userDTO.setPassword(password);
-        userDTO.setFirstName(firstname);
-        userDTO.setLastName(lastname);
-        userDTO.setAddress(address);
-        userDTO.setHeight(height);
-        userDTO.setDisplayName(displayName);
-        userDTO.setProfilePicture(profilePicture); // should be the bucket url
-        userDTO.setContact(contact);
-        userDTO.setPhoneNumber(phoneNumber);
-        userDTO.setBiography(biography);
-        userDTO.setPreferences(preferences);
-        userDTO.setDislikes(dislikes);
-        return userService.updateUser(userDTO);
-    }
-////
-    @GetMapping("/search")
-    public User search(@RequestParam(required = false) String q) {
-        return userService.search(q);
-    }
-
-    @GetMapping("/profile/{username}")
-    public String getProfile(@PathVariable String username) {
-        return userService.getProfile(username);
-    }
-}
+//@RestController
+//@RequestMapping("/api")
+//public class TestController {
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @Autowired
+//    private SimpleService simpleService;
+//
+//    @GetMapping("/user/{id}")
+//    public User getName(@PathVariable Long id) {
+//        return userService.getUser(id);
+//    }
+//
+//    @PostMapping("/user/create")
+//    @PreAuthorize("hasRole('USER')")
+//    public String createUser(
+//            @RequestParam String username,
+//            @RequestParam String firstname,
+//            @RequestParam String lastname,
+//            @RequestParam String address,
+//            @RequestParam String email,
+//            @RequestParam String password,
+//            @RequestParam String phone,
+//            @RequestParam int age,
+//            @RequestParam double height,
+//            @RequestParam String displayName,
+//            @RequestParam String profilePicture,
+//            @RequestParam String contact,
+//            @RequestParam String biography,
+//            @RequestParam Set<String> preferences,
+//            @RequestParam Set<String> dislikes
+//    ) {
+//        UserDTO userDTO = new UserDTO();
+//        userDTO.setUsername(username);
+//        userDTO.setEmail(email);
+//        userDTO.setPassword(password);
+//        userDTO.setFirstName(firstname);
+//        userDTO.setLastName(lastname);
+//        userDTO.setAddress(address);
+//        userDTO.setAge(age);
+//        userDTO.setHeight(height);
+//        userDTO.setDisplayName(displayName);
+//        userDTO.setProfilePicture(profilePicture); // should be the bucket url
+//        userDTO.setContact(contact);
+//        userDTO.setPhoneNumber(phone);
+//        userDTO.setBiography(biography);
+//        userDTO.setPreferences(preferences);
+//        userDTO.setDislikes(dislikes);
+//        return userService.createUser(userDTO);
+//    }
+//
+//    @PostMapping("/api/user/update") //TODO: can change the para, to restrict user to change stuff
+//    public String updateUser(
+//            @RequestParam String username,
+//            @RequestParam String firstname,
+//            @RequestParam String lastname,
+//            @RequestParam String address,
+//            @RequestParam String email,
+//            @RequestParam String password,
+//            @RequestParam String phoneNumber,
+//            @RequestParam double height,
+//            @RequestParam String displayName,
+//            @RequestParam String profilePicture,
+//            @RequestParam String contact,
+//            @RequestParam String biography,
+//            @RequestParam Set<String> preferences,
+//            @RequestParam Set<String> dislikes
+//    ) {
+//        UserDTO userDTO = new UserDTO();
+//        userDTO.setUsername(username);
+//        userDTO.setEmail(email);
+//        userDTO.setPassword(password);
+//        userDTO.setFirstName(firstname);
+//        userDTO.setLastName(lastname);
+//        userDTO.setAddress(address);
+//        userDTO.setHeight(height);
+//        userDTO.setDisplayName(displayName);
+//        userDTO.setProfilePicture(profilePicture); // should be the bucket url
+//        userDTO.setContact(contact);
+//        userDTO.setPhoneNumber(phoneNumber);
+//        userDTO.setBiography(biography);
+//        userDTO.setPreferences(preferences);
+//        userDTO.setDislikes(dislikes);
+//        return userService.updateUser(userDTO);
+//    }
+//////
+//    @GetMapping("/search")
+//    public User search(@RequestParam(required = false) String q) {
+//        return userService.search(q);
+//    }
+//
+//    @GetMapping("/profile/{username}")
+//    public String getProfile(@PathVariable String username) {
+//        return userService.getProfile(username);
+//    }
+//}
