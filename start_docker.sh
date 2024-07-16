5469e8b94160e132a26f9208ed9c5cbfb70b98f6
Lingling <pusanisa.jar@student.mahidol.edu>
2024-07-16 20:00:32 +0700

    add script for runing docker

---
+++ start_docker.sh
@@ -0,0 +1,9 @@
+docker run --name mariadb -d \
+  -p 3306:3306 \
+  --restart=always \
+  -v "$(pwd)/data":/var/lib/mysql \
+  -e MARIADB_ROOT_PASSWORD=devpass \
+  -e MARIADB_USER=dev \
+  -e MARIADB_PASSWORD=devpass \
+  -e MARIADB_DATABASE=orm \
+  mariadb:11.4.2-noble
