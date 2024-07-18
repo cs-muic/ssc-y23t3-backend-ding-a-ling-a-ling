3ac7fadd4f9fd280434a75c8a6e6ffef769e94bb
Lingling <pusanisa.jar@student.mahidol.edu>
2024-07-18 21:42:35 +0700

    ready to deploy

--- src/main/java/io/muzoo/ssc/springwebapp/config/SecurityConfig.java
+++ src/main/java/io/muzoo/ssc/springwebapp/config/SecurityConfig.java
@@ -25,7 +25,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-//    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
 
