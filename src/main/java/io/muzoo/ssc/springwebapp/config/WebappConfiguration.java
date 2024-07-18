6b237a3e85ab321e1d67842f861bc79d61cbe4af
Lingling <pusanisa.jar@student.mahidol.edu>
2024-07-18 17:16:34 +0700

    updated the project to be ready to deployed

--- src/main/java/io/muzoo/ssc/springwebapp/config/WebappConfiguration.java
+++ src/main/java/io/muzoo/ssc/springwebapp/config/WebappConfiguration.java
@@ -3,7 +3,6 @@ package io.muzoo.ssc.springwebapp.config;
 
 import io.muzoo.ssc.springwebapp.SimpleService;
 import io.muzoo.ssc.springwebapp.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
